ca757c77eb76f4fd28168d5116ec2047b7f990d2
Sam Lichlyter <slichlyter12@me.com>
2016-04-21 09:12:09 -0700

    Initial Commit

---
+++ Compass Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Compass Demo
+//
+//  Created by Samuel Lichlyter on 4/21/16.
+//  Copyright © 2016 Samuel Lichlyter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
